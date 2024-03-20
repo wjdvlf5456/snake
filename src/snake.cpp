c09a299d4c2b5ba078a6e8652251cb82ab659ea1
최정필 <wjdvlf1407@gmail.com>
2024-03-20 14:10:59 +0900

    snake 시작

---
+++ src/snake.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : snake.cpp
+// Author      : wjdvlf5456
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
